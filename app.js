b89c4de755dae47b0e9194d64263cebf7e4e76f1
Aashish Chachan <63456605+aashishchachan@users.noreply.github.com>
2021-05-28 02:33:37 +0530

    middleware

--- app.js
+++ app.js
@@ -1,18 +1,61 @@
 const express = require('express');
 const app = express();
 const fs= require('fs');
+const morgan = require('morgan')
 const port = 3000;
 
+app.use(morgan('dev'));
 app.use(express.json());
+app.use((req, res, next)=>{
+    console.log('Hello from the middleware')
+    req.requestTime = new Date().toISOString();
+})
 
 tours= JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+const getAllUsers = (req, res)=>{
+    res.status(500).json({
+        status: 'error',
+        message : 'not yet defined'
+    })
+}
+
+const getUserById = (req, res)=>{
+    res.status(500).json({
+        status: 'error',
+        message : 'not yet defined'
+    })
+}
+
+const createUser = (req, res)=>{
+    res.status(500).json({
+        status: 'error',
+        message : 'not yet defined'
+    })
+}
+
+const updateUserById = (req, res)=>{
+    res.status(500).json({
+        status: 'error',
+        message : 'not yet defined'
+    })
+}
+
+const deleteUserById = (req, res)=>{
+    res.status(500).json({
+        status: 'error',
+        message : 'not yet defined'
+    })
+}
+
+
 const getAllTours = (req,res) =>{
-    res.status(200)
-    .json({
+    res.status(200).json({
         status:'success',
+        requested: req.requestTime,
+        results: tours.length,
         data : {
             tours
         }
@@ -29,9 +72,10 @@ const getTourById = (req,res) =>{
             message: 'Invalid ID'
         });
     }
-    res.status(200)
-    .json({
+    res.status(200).json({
         status:'success',
+        requested: req.requestTime,
+        results: tours.length,
         data : {
             tour
         }
@@ -43,9 +87,10 @@ const createTour = (req, res) =>{
     const tour = Object.assign({id}, req.body);
     tours.push(tour)
     fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err=>{
-        res.status(200)
-        .json({
+        res.status(200).json({
             status:'success',
+            requested: req.requestTime,
+            results: tours.length,
             data : {
                 tours
             }
@@ -65,6 +110,8 @@ const updateTourById = (req, res)=>{
     }
     res.status(200).json({
         status: 'success',
+        requested: req.requestTime,
+        results: tours.length,
         data: {
             tour: '<updated tour>'
         }
@@ -83,17 +130,16 @@ const deleteTourById = (req, res)=>{
     }
     res.status(204).json({
         status: 'success',
+        requested: req.requestTime,
+        results: tours.length,
         data: null
     })
 }
 
-// app.get('/api/v1/tours', getAllTours);
-// app.get('/api/v1/tours/:id', getTourById);
-// app.post('/api/v1/tours', createTour);
-// app.patch('/api/v1/tours/:id', updateTourById);
-// app.delete('/api/v1/tours/:id', deleteTourById);
+const tourRouter = express.Router();
+const userRouter = express.Router();
 
-app.route('/api/v1/tours')
+app.route('/')
     .get(getAllTours)
     .post(createTour);
 
@@ -102,7 +148,18 @@ app.route('/api/v1/tours/:id')
     .patch(updateTourById)
     .delete(deleteTourById);
 
+app.route('/api/v1/users')
+    .get(getAllUsers)
+    .post(createUser);
 
+app.route('/api/v1/users/:id')
+    .get(getUserById)
+    .patch(updateUserById)
+    .delete(deleteUserById);
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+    
 
 app.listen(port, (req, res)=>{
     console.log(`listening at port ${port}`);
